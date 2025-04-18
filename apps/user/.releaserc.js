d77a596311a4f9dfbaedb38ab10749b8a772dbbc
Amith <amithpremnagandla@gmail.com>
2025-04-17 21:12:42 -0400

    fix: update releaserc.js

--- apps/user/.releaserc.js
+++ apps/user/.releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", "fix-semantic-release"], // Including your test branch
+  branches: ["main"],
   tagFormat: "user-v${version}",
   plugins: [
     "@semantic-release/commit-analyzer",
@@ -11,9 +11,15 @@ module.exports = {
       },
     ],
     [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md"], // Remove package.json from here
+        assets: ["CHANGELOG.md", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
